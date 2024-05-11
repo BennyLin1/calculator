4e3a2a7df65eaf75e95ec9c0b849fcc9857a16e3
BennyLin1 <79436149+BennyLin1users.no.reply.github.com>
2024-05-12 00:38:21 +1000

    Update: Basic functionality for calculator operations

--- scripts.js
+++ scripts.js
@@ -12,29 +12,85 @@ class Calculator {
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0,-1) //slice from start to second to last    
     }
 
     appendNumber(number){
-        this.currentOperand = number
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation){
-
+        if(this.currentOperand === '') return // Case where there is no digits
+        if(this.previousOperand !== '') { 
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const curr = parseFloat(this.currentOperand)
 
+        if (isNaN(prev) || isNaN(curr)) return 
+        switch(this.operation){
+            case '+':
+                computation = prev + curr
+                break
+            case '-':
+                computation = prev - curr
+                break
+            case 'x':
+                computation = prev * curr
+                break
+            case '÷':
+                computation = prev / curr
+                break
+            default:
+                return
+        }
+
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+
+        if (isNaN(integerDigits)){
+            integerDigits = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay(){
-        this.currentOperandElement.innerText = this.currentOperand
+        this.currentOperandElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) { 
+            this.previousOperandElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandElement.innerText = ''
+        }
     }
 }
 
+// Declare and initiate link between data attributes in HTML
 const numberButtons = document.querySelectorAll('[data-digits]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equal]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 
@@ -43,9 +99,35 @@ const currentOperandElement = document.querySelector('[data-curr-operand]')
 
 const calculator = new Calculator(previousOperandElement,currentOperandElement)
 
+// Assign functionality to corresponding buttons
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+// TODO: Fix functionality with negative numbers
+// TODO: Fix Display to properly display current status
